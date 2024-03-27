7d14b5cbff07a51eef765447fae3d035f87a12dc
Paulo Silva <paulo-cesar-batista@hotmail.com>
2024-03-27 08:17:37 +0000

    migrations using knex for movie notes was created.

--- SRC/controlers/UserController.js
+++ SRC/controlers/UserController.js
@@ -1,6 +1,6 @@
 const AppError = require('../utils/AppError');
 const sqliteConnection = require('../database/sqlite');
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 
 class UserController{
 
@@ -23,6 +23,60 @@ class UserController{
         return res.status(201).json("User created");
         
         
+    }
+
+    async update(req, res){
+        const { name, email, password, old_password } = req.body;
+        const { id } = req.params
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user){
+            throw new AppError("User not found");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+            throw new AppError("E-mail already in use");
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if(password && !old_password){
+
+            throw new AppError("You need to inform the old password to set a new password");
+        }
+
+        if(password && old_password){
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if(!checkOldPassword){
+                throw new AppError("Old password does not match");
+            }
+
+            user.password = await hash(password, 8);
+
+
+            
+        }
+        
+        await database.run(`
+                UPDATE users SET
+                name = ?,
+                email = ?,
+                password = ?,
+                updated_at = DATETIME('now')
+                WHERE id = ?`,
+                [user.name, user.email, user.password, id]
+         )
+
+        
+
+        
+        return res.status(200).json()
     }
 
 }
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+const path = require('path');
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -8,40 +9,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: path.resolve(__dirname, 'src', 'database','database.db')
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+      directory: path.resolve(__dirname, 'src', 'database','knex','migrations')
+    },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true
   }
 
 };
