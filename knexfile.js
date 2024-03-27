1ab1ef9b1061959874b61670af907e652a904046
Paulo Silva <paulo-cesar-batista@hotmail.com>
2024-03-27 08:39:23 +0000

    the migration for tag was created and also the pool function  that will allow to use the CASCADE command

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,12 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, 'src', 'database','database.db')
     },
+
+    pool:{ 
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)  
+      //it access the connection and callback function, this PRAGMA command allows the tags to be deleted with the cascade effect.
+         },
+     
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database','knex','migrations')
     },
